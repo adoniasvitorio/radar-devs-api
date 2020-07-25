8c77617cb4c5ac2f9b1a3972a304162701d8301d
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-25 00:13:10 -0300

    refactor: change the router schemas
    informed routes in an individual file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
+
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0.t2jhc.mongodb.net/omnistack-database?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 app.use(express.json());
-app.get('/', (request, response) =>{
-    return response.json({message: 'hello world'});
-});
-
+app.use(routes);
 app.listen(3333);
