a7cfb7895cca43637cbfa8fecdec1608625fa240
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-25 00:39:28 -0300

    feat: create route /devs
    this route search a user datas in github api.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 const { Router } = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
 routes.get('/', (request, response) =>{
-    return response.json({message: 'hello world'});
+    return response.json({ message: 'hello world' });
+});
+
+routes.post('/devs', async(request, response) =>{
+    
+    const { github_username } = request.body;
+    const api_response = await axios.get(`https://api.github.com/users/${github_username}`);
+    console.log(api_response.data);
+
+    return response.json({message: 'hello dev'});
 });
 
 module.exports = routes;
