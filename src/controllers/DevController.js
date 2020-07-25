ed5d29680e7702e798258a327597e86bcd7d45fc
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-25 01:31:06 -0300

    fix: duplicated register
    alter DevController to prevent duplicated registers in databases.

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -4,26 +4,28 @@ const Dev = require('../models/Dev');
 module.exports = {
     async store(request, response) {
 
-    const { github_username, techs, latitude, longitude } = request.body;
-    const techs_array = techs.split(',').map(techs => techs.trim());
+        const { github_username, techs, latitude, longitude } = request.body;
+        const techs_array = techs.split(',').map(techs => techs.trim());
 
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
+        let dev = await Dev.findOne({ github_username });
+        if (!dev) {
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+
+            const api_response = await axios.get(`https://api.github.com/users/${github_username}`);
+            const { name = login, avatar_url, bio } = api_response.data;
+
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techs_array,
+                location
+            });
+        }
+        return response.json(dev);
     }
-
-    const api_response = await axios.get(`https://api.github.com/users/${github_username}`);
-    const { name = login, avatar_url, bio } = api_response.data;
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techs_array,
-        location
-    });
-
-    return response.json(dev);
-}
 };
