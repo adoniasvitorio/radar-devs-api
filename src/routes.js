5e09a2b3fa7927cd70a7a9e8cff1593b543b7985
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-25 01:23:55 -0300

    refactor: abstract dev controller
    create controller to include a new devs

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
@@ -8,29 +7,6 @@ routes.get('/', (request, response) =>{
     return response.json({ message: 'hello world' });
 });
 
-routes.post('/devs', async(request, response) =>{
-    
-    const { github_username, techs, latitude, longitude } = request.body;
-    const techs_array = techs.split(',').map(techs => techs.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
-    }
-
-    const api_response = await axios.get(`https://api.github.com/users/${github_username}`);
-    const { name = login, avatar_url, bio } = api_response.data;
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techs_array,
-        location
-    });
-
-    return response.json(dev);
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
