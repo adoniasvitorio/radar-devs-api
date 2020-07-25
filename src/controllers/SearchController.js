89b2c8f242afb107ab19e14ef584ed5365aaf950
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-25 02:31:50 -0300

    feat: filter devs by distance
    include in filter devs with location near from 10000 mts.

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -5,11 +5,20 @@ module.exports = {
     async index(request, response) {
         const { latitude, longitude, techs } = request.query;
         const techs_array = parseStringAsArray(techs);
-       
+
         const devs = await Dev.find({
             techs: {
                 $in: techs_array,
-            }
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                },
+            },
         });
         return response.json({ devs });
     }
