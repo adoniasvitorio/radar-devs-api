fec2facab57ccc4ee9a3a6266e34a8816bf45a03
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-25 02:00:59 -0300

    feat: new route search
    create a new route to search devs

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
@@ -9,5 +10,8 @@ routes.get('/', (request, response) =>{
 
 routes.post('/devs', DevController.store);
 routes.get('/devs', DevController.index);
+
+routes.get('/search', SearchController.index);
+
 
 module.exports = routes;
