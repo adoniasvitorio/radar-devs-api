7956d6d644c7e75ce1bc7fc1ecb402105ff1f6c1
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-24 23:45:56 -0300

    feat: add mongoose and connect mongo db database
    create cluster in MogoDB Atlas and config in project.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { response } = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0.t2jhc.mongodb.net/omnistack-database?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
+app.use(express.json());
 app.get('/', (request, response) =>{
     return response.json({message: 'hello world'});
 });
